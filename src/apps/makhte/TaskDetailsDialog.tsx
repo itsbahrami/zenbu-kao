117741540fc7ac46256ad83fe70a9509452bcd7c
Bahrami <m.m.bhrm85@gmail.com>
2026-09-08 15:37:12 +0330

    Start to use Makhte and do a task.

--- src/apps/makhte/TaskDetailsDialog.tsx
+++ src/apps/makhte/TaskDetailsDialog.tsx
@@ -39,11 +39,11 @@ export function TaskDetailsDialog() {
           </DialogHeader>
 
           <div className='flex flex-col gap-2'>
-            <h3 className='text-lg font-bold'>{task.title}</h3>
+            <h3 dir='auto' className='text-lg font-bold'>{task.title}</h3>
 
             <TaskStatusBadge status={task.status} />
 
-            <p className='text-sm text-muted-foreground'>
+            <p dir='auto' className='text-sm text-muted-foreground'>
               {task.description || '(بدون توضیحات)'}
             </p>
           </div>
