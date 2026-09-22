d91da82c940d1d2fa787f688a0b7d734e4006726
Bahrami <m.m.bhrm85@gmail.com>
2026-09-22 15:52:39 +0330

    WIP designing landing page.

--- src/apps/blueprint/LandingPage.tsx
+++ src/apps/blueprint/LandingPage.tsx
@@ -1,5 +1,25 @@
-export const LandingPage = () => {
-  return <div>
+import c from './content.json'
 
+export const LandingPage = () => {
+  return <div className="bg-blue-950 min-h-dvh">
+    <HeroSection />
   </div>
 }
+
+export const HeroSection = () => (
+  <header className='flex flex-col items-center text-center py-16'>
+    <h1 className='font-heading font-black text-4xl whitespace-pre leading-relaxed'>{c.hero_title}</h1>
+
+    <p className='font-sans'>{c.hero_description}</p>
+
+    <div className="">
+      <button type="button" className=''>
+        <span className=''></span>
+      </button>
+
+      <button type="button" className=''>
+        <span className=''></span>
+      </button>
+    </div>
+  </header>
+)
