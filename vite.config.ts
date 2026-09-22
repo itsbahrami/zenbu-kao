4418a84ef6f85b2e6e8d4b93c15003192b71f7ad
Bahrami <m.m.bhrm85@gmail.com>
2026-09-22 15:42:27 +0330

    Config SPA paths to not be pre-rendered.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import viteReact from '@vitejs/plugin-react'
 import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
 
+const SPA_PATHS = [
+  '/dashboard',
+  '/apps',
+  '/login',
+  '/register',
+  '/contact',
+]
+
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
@@ -15,22 +23,10 @@ const config = defineConfig({
         enabled: true,
         autoStaticPathsDiscovery: true,
         crawlLinks: true,
+        filter: p => !SPA_PATHS.some(spaPath => spaPath === p.path || p.path.startsWith(spaPath)),
       },
     }),
-    nitro({
-      prerender: {
-        routes: ['/'],
-        ignore: [
-          '/apps/**',
-          '/dashboard/**',
-          '/contact',
-          '/login',
-          '/register',
-          '/privacy',
-          '/terms',
-        ],
-      },
-    }),
+    nitro(),
     viteReact(),
   ],
 })
