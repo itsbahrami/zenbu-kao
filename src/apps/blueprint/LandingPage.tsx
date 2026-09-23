d7eb7345e8d275149907564e2bdb481378fccf21
gikdev <m.m.bhrm85@gmail.com>
2026-09-23 04:20:39 +0330

    Write the "with" section

--- src/apps/blueprint/LandingPage.tsx
+++ src/apps/blueprint/LandingPage.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from './HeroSection'
 import { WhatSection } from './WhatSection'
+import { WithSection } from './WithSection'
 
 export const LandingPage = () => (
   <div className='min-h-dvh leading-relaxed'>
     <HeroSection />
     <WhatSection />
+    <WithSection />
   </div>
 )
---
+++ src/apps/blueprint/WithSection.tsx
@@ -0,0 +1,48 @@
+import { CheckCircleIcon, CheckIcon, XCircleIcon, XIcon } from '@phosphor-icons/react';
+import c from './content.json';
+
+export const WithSection = () => (
+  <header className='bg-mist-50 flex flex-col gap-4 py-12 px-4 text-mist-700'>
+    <h1 className='text-mist-900 text-center font-heading font-black text-xl sm:text-2xl leading-relaxed'>
+      {c.with.title}
+    </h1>
+
+    <p className='font-sans text-center'>{c.with.description}</p>
+
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+      <div className='flex flex-col gap-2 items-start p-2 sm:p-4 rounded-md bg-red-100 transition-all border border-red-300'>
+        <div className="flex items-center gap-2">
+          <XCircleIcon size={24} className='text-red-500' weight='duotone' />
+
+          <p className='font-heading text-mist-900 font-bold'>بدون بلوپرینت</p>
+        </div>
+
+        <div className='flex flex-col gap-1'>
+          {c.with.without.map(item => (
+            <p key={item} className=''>
+              <XIcon size={20} className='text-red-500 inline-block me-1' />
+              <span>{item}</span>
+            </p>
+          ))}
+        </div>
+      </div>
+
+      <div className='flex flex-col gap-2 items-start p-2 sm:p-4 rounded-md bg-emerald-100 transition-all border border-emerald-300'>
+        <div className="flex items-center gap-2">
+          <CheckCircleIcon size={24} className='text-emerald-500' weight='duotone' />
+
+          <p className='font-heading text-mist-900 font-bold'>با بلوپرینت</p>
+        </div>
+
+        <div className='flex flex-col gap-1'>
+          {c.with.with.map(item => (
+            <p key={item} className=''>
+              <CheckIcon size={20} className='text-emerald-500 inline-block me-1' />
+              <span>{item}</span>
+            </p>
+          ))}
+        </div>
+      </div>
+    </div>
+  </header>
+)
